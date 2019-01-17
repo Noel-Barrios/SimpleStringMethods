e7d9e918455d6504fbf01de4ca6f83040b5319b3
Noel Barrios <noel.barrios@optum.com>
2019-01-17 14:43:45 -0600

    use Trim methods

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleStringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string greeting = "     Hello World!     ";
+            Console.WriteLine($"[{greeting}]");
+
+            //Now trim the spaces at the beginning of the greeting string
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            //now trim the spaces at the end of the greeting string
+            trimmedGreeting = trimmedGreeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+
+
+
+            // using the Trim() method would essentially do the same thing.
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+
+
+        }
+    }
+}
